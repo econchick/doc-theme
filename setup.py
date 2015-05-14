6a1ddeb68193b201f64baf08f377fedc413e6c1f
Daniel D. Beck <me@danieldbeck.com>
2015-05-13 21:42:10 -0400

    remove extraneous spaces from setup() call

--- setup.py
+++ setup.py
@@ -11,18 +11,12 @@ with codecs.open(readme_path, encoding='utf-8') as f:
 
 setup(
     name='pyladies_sphinx_theme',
-
     version='0.1.dev0',
-
     description='A PyLadies theme for Sphinx',
     long_description=long_description,
-
     url='https://github.com/pyladies/doc-theme',
-
     author='PyLadies',
-
     license='MIT',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Sphinx :: Theme',
@@ -33,18 +27,13 @@ setup(
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
     ],
-
     keywords='pyladies sphinx theme documentation docs',
-
     packages=['pyladies_sphinx_theme'],
-
     install_requires=['sphinx'],
-
     package_data={'pyladies_sphinx_theme': [
         'theme.conf',
         '*.html',
         'static/css/*.css',
     ]},
-
     zip_safe=False,
 )
